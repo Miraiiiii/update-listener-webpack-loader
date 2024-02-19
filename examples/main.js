6aa9bf8d991fbcb9804ca7ab1ee3082c0c740237
shehong <shehong@qingmutec.com>
2024-02-19 17:10:31 +0800

    UPDATE-240219 ci(lib)：打包

--- examples/main.js
+++ examples/main.js
@@ -3,7 +3,7 @@
  * @Author: 舌红
  * @Date: 2024-01-10 17:00:02
  * @LastEditors: 舌红
- * @LastEditTime: 2024-02-19 14:14:44
+ * @LastEditTime: 2024-02-19 16:54:53
  */
 import Vue from 'vue'
 import App from './App.vue'
@@ -11,9 +11,7 @@ import ListenerLoader from '../packages'
 
 Vue.config.productionTip = false
 
-Vue.use(ListenerLoader, {
-  type: 'qingmu'
-})
+Vue.use(ListenerLoader, {})
 
 new Vue({
   render: h => h(App),
